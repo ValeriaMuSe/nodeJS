71ba1d1d71ad27ba9d0601b533bce581dd0a3477
Vale <valeriams13@gmail.com>
2023-06-06 19:57:03 -0600

    Quiz1

---
+++ quiz1/app.js
@@ -0,0 +1,4 @@
+const functions = require('./funciones')
+
+console.log(functions.maxNumber())
+console.log(functions.minNumber())
---
+++ quiz1/funciones.js
@@ -0,0 +1,49 @@
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+  // Devuelve el mayor número de un array
+function maxNumber() {
+
+  let result = 0;
+  // Recorremos el array pa obtener el valor mayor
+  for(var i=0; i< array.length; i++){
+      let number = array[i];
+      // En la primera vuelta cogemos el valor
+      if(i == 0){
+          result = number;
+      }
+      // En las demás cogemos el valor si es mayor al que tenemos
+      else{
+          if(number > result){
+              result = number;
+          }
+      }
+  }
+  // Siempre devuelve el mayor de todos
+  return result;
+}
+
+function minNumber() {
+  
+  let result = 0;
+  // Recorremos el array pa obtener el valor mayor
+  for(var i=0; i< array.length; i++){
+      let number = array[i];
+      // En la primera vuelta cogemos el valor
+      if(i == 0){
+          result = number;
+      }
+      // En las demás cogemos el valor si es mayor al que tenemos
+      else{
+          if(number < result){
+              result = number;
+          }
+      }
+  }
+  // Siempre devuelve el mayor de todos
+  return result;
+}
+
+module.exports = {
+  maxNumber: maxNumber,
+  minNumber: minNumber,
+}
